a70e96242d78b43503f9a2e6125507c8f379c42e
Anastasiia  Yushko <47480757+anastasiiayushko@users.noreply.github.com>
2026-01-28 23:03:14 +0200

    27 топ

--- src/modules/quiz/quiz-game/infrastructure/query/game-statistic.query-repository.ts
+++ src/modules/quiz/quiz-game/infrastructure/query/game-statistic.query-repository.ts
@@ -50,8 +50,6 @@ export class GameStatisticQueryRepository {
       });
     }
 
-    console.log('sql', statisticQb.getSql());
-
     const [result, totalCount] = await statisticQb
       .skip((+queryParamsDto.pageNumber - 1) * queryParamsDto.pageSize)
       .take(queryParamsDto.pageSize)
